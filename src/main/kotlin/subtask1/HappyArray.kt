bbb3aa1ad94e26da21be2f093af5d661ab101073
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-28 17:40:30 +0300

    feat: HappyArray.convertToHappy method

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,36 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val sadArraySize = sadArray.size
+
+        if (sadArraySize < 3) {
+            return sadArray
+        }
+
+        val resultArray = sadArray.toMutableList();
+
+        var index = 1;
+        var interation = 1;
+        var iterationsAmount = sadArraySize - 1;
+//        intArrayOf(56, 32, 5, 11, 43, 62)
+        while (interation < iterationsAmount) {
+            val value = resultArray[index];
+            val neighboursSum =  resultArray[index - 1] + resultArray[index + 1];
+
+            if (value > neighboursSum) {
+                resultArray.removeAt(index);
+
+                if (index > 1) {
+                    iterationsAmount++;
+                    index--;
+                }
+            } else {
+                index++;
+            }
+
+            interation++;
+        }
+
+        return resultArray.toIntArray()
     }
 }
