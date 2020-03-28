c9d15ac83b38671358dda18bb9c25ddfc47fce3f
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-28 18:13:18 +0300

    feat: BillCounter.calculateFairlySplit method

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -4,7 +4,19 @@ class BillCounter {
 
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
-    fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+    fun calculateFairlySplit(bill: IntArray, allergicIndex: Int, annaPaid: Int): String {
+        val excludedAllergicBill = bill.toMutableList();
+        excludedAllergicBill.removeAt(allergicIndex);
+
+        val sumToSplit = excludedAllergicBill.reduce { acc, price -> acc + price };
+
+        val annaShouldPay = sumToSplit / 2;
+
+        return if (annaPaid === annaShouldPay) {
+            "Bon Appetit"
+        } else {
+            val annaOverpaid = annaPaid - annaShouldPay;
+            "$annaOverpaid"
+        }
     }
 }
