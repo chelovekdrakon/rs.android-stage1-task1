522d60a8c7a24240f6bba87b26d4deba98497960
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-29 18:24:00 +0300

    feat: StringParser.getResult method

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -1,9 +1,37 @@
 package subtask4
 
 class StringParser {
+    private val mapOpenBracketToClosed = hashMapOf(
+        "(" to ")",
+        "[" to "]",
+        "<" to ">"
+    )
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result = mutableListOf<String>()
+
+        val string = StringBuilder(inputString)
+
+        for (i in (string.length - 1) downTo 0) {
+            val closedBracketMatch = mapOpenBracketToClosed[string[i].toString()];
+
+            if (closedBracketMatch != null) {
+                var lastBracketIndex = 0
+
+                for (j in i until string.length) {
+                    if (string[j].toString() == closedBracketMatch) {
+                        lastBracketIndex = j
+                        string[i] = '_'
+                        string[lastBracketIndex] = '_'
+                        break
+                    }
+                }
+
+                result.add(inputString.slice(IntRange(i + 1, lastBracketIndex - 1)))
+            }
+        }
+
+        return result.toTypedArray().reversedArray()
     }
 }
